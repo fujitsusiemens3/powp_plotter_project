1020ce48395510e1865f94d8e00932a93da5b1aa
Cezary Witkowski <czarecoo@gmail.com>
2018-03-15 15:42:14 +0100

    added CommandDrawLineToPosition class

---
+++ src/main/java/edu/iis/powp/command/CommandDrawLineToPosition.java
@@ -0,0 +1,19 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class CommandDrawLineToPosition implements PlotterCommand {
+	
+	private int x;
+	private int y;
+	
+	public CommandDrawLineToPosition() {
+	
+	}
+	
+	@Override
+	public void execute(IPlotter plotter) {
+		
+	}
+	
+}
